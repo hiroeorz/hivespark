2b710c1ab19236201313e377c190248ea2a200dd
HIROE Shin <hiroe.orz@gmail.com>
2012-05-17 14:42:43 +0900

    add debug output.

--- src/notification_handler.erl
+++ src/notification_handler.erl
@@ -46,6 +46,7 @@ websocket_init(_TransportName, Req, _Opts) ->
 %%%===================================================================
 %% 一定時間通信が無いと接続を遮断するルータ対策。ブラウザと１分に１回やりとりする。
 websocket_handle({text, <<"TICK">>}, Req, State) ->
+    ?debugVal("TICK"),
     Reply = jiffy:encode({[{<<"type">>, <<"keep_alive">>}, 
                            {<<"data">>, <<"TACK">>}]}),
     {reply, {text, Reply}, Req, State};
